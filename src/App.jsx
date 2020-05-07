ae36c625ef897b75e363544d5367f287d20b8427
kavi <kaviathelete@gmail.com>
2020-05-08 03:07:59 +0530

    'InitialCommit'

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navigation from "./components/Navigation/Navigation";
+import ChangePassword from "./components/ChangePassword/ChangePassword";
+function App() {
+	return (
+
+		<div className="App">
+			<Navigation/>
+			<ChangePassword/>
+		</div>
+	);
+}
+
+export default App;
---
+++ src/components/cart/cart.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+export class Cart extends Component {
+    render() {
+        return (
+            <div className="cart">
+                
+                <div className="cart__products">
+                    
+                </div>
+            
+                <div className="cart__estimate">
+                    
+                </div>
+            
+            </div>
+        )
+    }
+}
+
+export default Cart
